b36bcf6f626446009a07433f7e4d14409f83c427
sofwan118140052 <sofwan.118140052@student.itera.ac.id>
2021-05-29 10:56:48 +0700

    first commit

---
+++ src/Form.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import './Form.css';
+import FormSignup from './FormSignup';
+import FormSuccess from './FormSuccess';
+
+const Form = () => {
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  function submitForm() {
+    setIsSubmitted(true);
+  }
+  return (
+    <>
+      <div className='form-container'>
+        <span className='close-btn'>×</span>
+        <div className='form-content-left'>
+          <img className='form-img' src='https://cdn.idntimes.com/content-images/community/2020/07/53421265-138489060530399-103199218608235931-n-6d903e5c46aa02d7a406ff387486fb80.jpg' alt='spaceship' />
+        </div>
+        {!isSubmitted ? (
+          <FormSignup submitForm={submitForm} />
+        ) : (
+          <FormSuccess />
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Form;
